1449ac49fefef82f3e600d7fc40c23fc3e8af414
Leonardo Benevenuto <leonardo.web02@gmail.com>
2019-01-15 19:50:40 -0200

    atualizacoes

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
+use \Hcode\Model\Category;
 
 $app = new Slim();
 
@@ -91,6 +92,15 @@ $app->get("/admin/users/:iduser/delete", function($iduser){
 
 	User::verifyLogin();
 
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$user->delete();
+
+	header ("Location: /admin/users");
+	exit;
+
 
 });
 
@@ -136,8 +146,170 @@ $app->post("/admin/users/:iduser", function($iduser){
 
 	User::verifyLogin();
 
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"])) ? 1 : 0;
+
+	$user->get((int)$iduser);
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header ("Location: /admin/users");
+	exit;
+
 
 });
+
+
+
+$app->get("/admin/forgot", function(){
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot");
+
+});
+
+
+$app->post("/admin/forgot", function(){
+
+	$user = User::getForgot($_POST["email"]);
+
+	header ("Location: /admin/forgot/sent");
+	exit;
+
+});
+
+
+$app->get("/admin/forgot/sent", function(){
+
+	 $page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-sent");
+
+});
+
+
+$app->get('/admin/forgot/reset', function(){
+
+	$user = User::validForgotDecrypt($_GET["code"]);
+
+
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("forgot-reset", array(
+		"name"=>$user["desperson"],
+		"code"=>$user["code"]
+	));
+
+});
+
+
+$app->get("/admin/categories", function(){
+
+	User::verifyLogin();
+
+	$categories = Category::listAll();
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories", [
+
+		'categories'=>$categories
+
+	]);
+
+});
+
+
+$app->get("/admin/categories/create", function(){
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-create");
+
+});
+
+
+$app->post("/admin/categories/create", function(){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
+
+});
+
+
+$app->get("/admin/categories/:idcategory/delete", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->delete();
+
+	header ("Location: /admin/categories");
+	exit;
+
+});
+
+
+$app->get("/admin/categories/:idcategory", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new PageAdmin();
+
+	$page->setTpl("categories-update", array(
+		"category"=>$category->getValues()
+	));
+
+});
+
+
+$app->post("/admin/categories/:idcategory", function($idcategory){
+
+	User::verifyLogin();
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$category->setData($_POST);
+
+	$category->save();
+
+	header ("Location: /admin/categories");
+	exit;
+
+
+});
+
+
 
 
 
--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -4,11 +4,13 @@ namespace Hcode\Model;
 
 use \Hcode\DB\Sql;
 use \Hcode\Model;
+use \Hcode\Mailer;
 
 
 class User extends Model {
 
 	const SESSION = "User";
+	const SECRET = "HcodePhp7_secret";
 
 	public static function login($login, $password){
 
@@ -110,11 +112,149 @@ class User extends Model {
 			":iduser"=>$iduser
 		));
 
-		$this->setData($results);
+		$this->setData($results[0]);
+	}
+
+
+
+
+	public function update()
+	{
+		$sql = new Sql();
+
+		$results = $sql->select("CALL sp_usersupdate_save(:iduser, :desperson, :deslogin, :despassword, :desemail, :nrphone, :inadmin)", array(
+			":iduser"=>$this->getiduser(),
+			":desperson"=>$this->getdesperson(),
+			":deslogin"=>$this->getdeslogin(),
+			":despassword"=>$this->getdespassword(),
+			":desemail"=>$this->getdesemail(),
+			":nrphone"=>$this->getnrphone(),
+			":inadmin"=>$this->getinadmin()
+		));
+
+		$this->setData($results[0]);
+	}
+
+
+
+
+
+	public function delete()
+	{
+		$sql = new Sql();
+
+		$sql->query("CALL sp_users_delete(:iduser)", array(
+			"iduser"=>$this->getiduser()
+		));
+	}
+
+
+
+
+	public static function getForgot($email, $inadmin = true)
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * from tb_persons a INNER JOIN tb_users b USING(idperson) WHERE a.desemail = :email", array(
+			":email"=>$email
+		));
+
+
+		if (count($results) === 0)
+		{
+
+			throw new \Exception("Não foi possível recuperar a senha");
+
+		} else {
+
+			$data = $results[0];
+
+			$results2 = $sql->select("CALL sp_userspasswordsrecoveries_create(:iduser, :desip)", array(
+				":iduser"=>$data["iduser"],
+				":desip"=>$_SERVER["REMOTE_ADDR"]
+			));
+
+			if(count($results2) === 0)
+			{
+				throw new \Exception("Não foi possível recuperar a senha");
+			
+			} else {
+
+				$dataRecovery = $results2[0];
+
+				 $iv = random_bytes(openssl_cipher_iv_length('aes-256-cbc'));
+                 $code = openssl_encrypt($dataRecovery['idrecovery'], 'aes-256-cbc', User::SECRET, 0, $iv);
+                 $result = base64_encode($iv.$code);
+
+				if ($inadmin === true)
+				{
+					$link = "http://www.hcodecommerce.com.br/admin/forgot/reset?code=$code";
+				} else 
+				{
+					$link = "http://www.hcodecommerce.com.br/forgot/reset?code=$code";
+				}
+
+
+				$mailer = new Mailer($data["desemail"], $data["desperson"], "Redefinir senha HCODE store", "forgot", array(
+
+					"name"=>$data["desperson"],
+					"link"=>$link
+				));
+
+
+				$mailer->send();
+
+				return $data;
+
+
+			}
+
+		}
+
+	}
+
+
+
+
+	public static function validForgotDecrypt($result){
+
+
+		$result = base64_decode($result);
+     	$code = mb_substr($result, openssl_cipher_iv_length('aes-256-cbc'), null, '8bit');
+     	$iv = mb_substr($result, 0, openssl_cipher_iv_length('aes-256-cbc'), '8bit');;
+     	$idrecovery = openssl_decrypt($code, 'aes-256-cbc', User::SECRET, 0, $iv);
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT *
+								FROM tb_userspasswordsrecoveries a
+								INNER JOIN tb_users b USING(iduser)
+								INNER JOIN tb_persons c USING(idperson)
+								WHERE
+									a.idrecovery = :idrecovery
+								    AND
+								    a.dtrecovery IS NULL
+								    AND
+								    DATE_ADD(a.dtregister, INTERVAL 1 HOUR) >= NOW()
+								    ", array(
+								    	":idrecovery"=>$idrecovery
+								    ));
+
+		if(count($results) === 0)
+		{
+			throw new \Exception("Não foi possível recuperar a senha");
+		} else {
+
+			return $results[0];
+		}
+
 	}
 
 
 }
+
+
 
 
 ?>
