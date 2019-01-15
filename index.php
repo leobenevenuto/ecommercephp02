362d86b45a44ebc07bda00458aee0ba96370e7af
Leonardo Benevenuto <leonardo.web02@gmail.com>
2019-01-15 20:16:34 -0200

    site categorias

--- index.php
+++ index.php
@@ -291,6 +291,7 @@ $app->get("/admin/categories/:idcategory", function($idcategory){
 });
 
 
+
 $app->post("/admin/categories/:idcategory", function($idcategory){
 
 	User::verifyLogin();
@@ -306,6 +307,21 @@ $app->post("/admin/categories/:idcategory", function($idcategory){
 	header ("Location: /admin/categories");
 	exit;
 
+
+});
+
+$app->get("/categories/:idcategory", function($idcategory){
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
+	$page = new Page();
+
+	$page->setTpl("category", array(
+		"category"=>$category->getValues(),
+		"products"=>[]
+	));
 
 });
 
